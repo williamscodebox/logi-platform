43cd5c53c1ddc4c6eb80d1cfe7515618d3078946
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-04-16 14:49:37 -0400

    update prisma config

--- packages/prisma/prisma.config.ts
+++ packages/prisma/prisma.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   schema: './schema.prisma', 
   migrations: {
     path: "prisma/migrations",
+    seed: 'ts-node ./seed.ts',
   },
   datasource: {
     url: env('DATABASE_URL'),
