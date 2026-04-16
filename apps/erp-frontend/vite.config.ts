c4cbbaf74d40a1e4c7a453438b80f608a1bf0fb1
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-04-16 17:32:09 -0400

    update vite config

--- apps/erp-frontend/vite.config.ts
+++ apps/erp-frontend/vite.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig({  
+  server: {
+    proxy: {
+      '/dashboard': {
+        target: 'http://localhost:3000', // your NestJS backend
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [react()],
 })
