e4c7dd4bda1834d46e970a60b38e31e312a3f26a
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-04-21 19:53:56 -0400

    update dashboard

--- apps/api/src/Dashboard/dashboard.module.ts
+++ apps/api/src/Dashboard/dashboard.module.ts
@@ -3,12 +3,13 @@ import { Module } from '@nestjs/common';
 import { DashboardController } from './dashboard.controller';
 import { DashboardService } from './dashboard.service';
 import { OrdersModule } from '../Orders/orders.module';
+import { InventoryModule } from '../Inventory/inventory.module';
 
 @Module({
   imports: [
     OrdersModule,
     // ShipmentsModule,
-    // InventoryModule,
+    InventoryModule,
   ],
 
   controllers: [DashboardController],
--- apps/api/src/Dashboard/dashboard.service.ts
+++ apps/api/src/Dashboard/dashboard.service.ts
@@ -1,24 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { OrdersRepository } from '../Orders/orders.repository';
 // import { ShipmentsRepository } from './shipments.repository';
-// import { InventoryRepository } from './inventory.repository';
+import { InventoryRepository } from '../Inventory/inventory.repository';
 
 // dashboard.service.ts
 @Injectable()
 export class DashboardService {
-  constructor(private readonly ordersRepo: OrdersRepository) {}
-  //   private readonly shipmentsRepo: ShipmentsRepository,
-  //   private readonly inventoryRepo: InventoryRepository,
-
+  constructor(
+    private readonly ordersRepo: OrdersRepository,
+    private readonly inventoryRepo: InventoryRepository,
+  ) {}
+  //   private readonly shipmentsRepo: ShipmentsRepository
   async getSummary() {
     const { allOrders } = await this.ordersRepo.countOpenOrders();
     // const lateShipments = await this.shipmentsRepo.countLateShipments();
-    // const lowInventory = await this.inventoryRepo.countLowStock();
+    const lowInventory = await this.inventoryRepo.countLowStock();
 
     return {
       allOrders,
+      lowInventory,
       // lateShipments,
-      // lowInventory,
     };
   }
 }
