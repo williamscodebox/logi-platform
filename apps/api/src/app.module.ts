a65affcd5371d20f9e2dbb940e2bf56310066cae
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-04-21 06:41:23 -0400

    update app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { DashboardModule } from './Dashboard/dashboard.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    DashboardModule,
     // InventoryModule,
     // OrdersModule,
     // ShipmentsModule,
