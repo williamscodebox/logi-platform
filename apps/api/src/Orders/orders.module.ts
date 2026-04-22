0257603a4358fe65783560f69d5bb22e4d0e2788
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2026-04-21 20:51:25 -0400

    updated orders module

--- apps/api/src/Orders/orders.module.ts
+++ apps/api/src/Orders/orders.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { OrdersRepository } from './orders.repository';
 import { PrismaModule } from '../prisma/prisma.module';
+import { OrdersController } from './orders.controller';
+import { OrdersService } from './orders.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [OrdersRepository],
+  providers: [OrdersRepository, OrdersService],
   exports: [OrdersRepository],
+  controllers: [OrdersController],
 })
 export class OrdersModule {}
